8551a537d5f82b1506ec2dc4eaf37eeb1d376724
marynovski <marynovski@gmail.com>
2018-11-07 13:14:14 +0100

    Initial commit

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+
+
+header('Location: pages/strona-główna');
+
+
+
+?>
---
+++ pages/orders.php
@@ -0,0 +1,19 @@
+<html>
+
+<body>
+
+<h1><?php echo __FILE__; ?></h1>
+
+<a href="strona-główna">Główna</a>
+<a href="rejestracja">Rejestracja</a>
+<a href="klienci">Klienci</a>
+<a href="zamówienia">Zamówienia</a>
+<a href="profil">Profil</a>
+<a href="produkty">Produkty</a>
+
+
+
+</body>
+
+
+</html>
